aa023824a20d3acd7a4c3a23402754fb454751b0
Dmitrii Kushchenko <dikushch@gmail.com>
2024-03-11 13:22:08 +0300

    feat: add removeroperties function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -66,8 +66,14 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const newObj = obj;
+  keys.forEach((key) => {
+    if (Object.prototype.hasOwnProperty.call(newObj, key)) {
+      delete newObj[key];
+    }
+  });
+  return newObj;
 }
 
 /**
